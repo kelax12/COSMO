27b2df4ddb83b8dd3d263b58c58d6c823b7ef8dc
kelax12 <kelaxbusiness@gmail.com>
2026-03-04 23:23:11 +0100

    Update repository.ts

--- src/modules/habits/repository.ts
+++ src/modules/habits/repository.ts
@@ -2,6 +2,7 @@ import { Habit, CreateHabitInput, UpdateHabitInput } from './types';
 
 export interface IHabitsRepository {
   fetchHabits(): Promise<Habit[]>;
+  getById(id: string): Promise<Habit | null>;
   createHabit(habit: CreateHabitInput): Promise<Habit>;
   updateHabit(id: string, updates: UpdateHabitInput): Promise<Habit>;
   deleteHabit(id: string): Promise<void>;
