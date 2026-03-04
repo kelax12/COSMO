1ca36562aef30c858d9a7fa5244d5a99665ca530
kelax12 <kelaxbusiness@gmail.com>
2026-03-04 21:17:41 +0100

    Update AgendaPage.tsx

--- src/pages/AgendaPage.tsx
+++ src/pages/AgendaPage.tsx
@@ -395,8 +395,8 @@ setShowTaskSidebar(false);
                     </motion.button>
                   </div>
 
-                  <div className="flex rounded-lg p-0.5 col-span-2 lg:col-span-1 lg:w-auto" style={{ backgroundColor: 'rgb(var(--color-hover))' }}>
-                    {['timeGridDay', 'timeGridWeek', 'dayGridMonth'].map((view, index) =>
+                  <div className=\"flex rounded-lg p-0.5 col-span-2 lg:col-span-1 lg:w-auto\" style={{ backgroundColor: 'rgb(var(--color-hover))' }}>
+                    {['timeGridDay', 'timeGridWeek', 'dayGridMonth'].map((view, _index) =>
                       <motion.button
                         key={view}
                         onClick={() => handleViewChange(view)}
